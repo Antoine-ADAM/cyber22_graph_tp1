fd516cff3a307e476609cc28c96dc7e53c543a7d
Antoine ADAM <flancraft10@gmail.com>
2022-09-25 12:13:20 +0200

    add comment

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 from random import randint
 from typing import List, Set, Optional, Tuple
-from logging import info
+from logging import info, debug
 
 
 class Graph:
@@ -11,7 +11,11 @@ class Graph:
         self.is_oriented = is_oriented
 
     def add_vector(self, source: int, destination: int, weight: Optional[int]) -> bool:
+        # Check if the vector points to itself
         if source == destination:
+            return False
+        # Check if the vector already exists
+        if (destination, weight) in self.vectors[source]:
             return False
         self.vectors[source].add((destination, weight))
         if not self.is_oriented:
@@ -24,55 +28,76 @@ class Graph:
             self.labels.append(label)
         return len(self.vectors) - 1
 
-    def dominating_set(self):
-        info("")
+    def dominating_set(self) -> Set[int]:
+        info("[DOMINATING SET] Initializing...")
         res = set()
         inverse_res = set(range(len(self.vectors)))
         match_node = inverse_res.copy()
+        info("[DOMINATING SET] Starting...")
         while match_node:
             max_node = -1
             max_ = -1
+            debug(f"[DOMINATING SET] {len(match_node)} nodes not targeted")
             for node in inverse_res:
                 count = 1 if node in match_node else 0
+                # score = 0 if node is targeted else 1
                 for children in self.vectors[node]:
+                    # add 1 if the child is not targeted
                     if children[0] in match_node:
                         count += 1
+                # compare the score
                 if count > max_:
                     max_ = count
                     max_node = node
-
+            debug(f"[DOMINATING SET] {max_node} is the best node, with {max_} children targeted")
+            # add the node to the result
             res.add(max_node)
             inverse_res.discard(max_node)
             match_node.discard(max_node)
+            # remove the children in the not targeted nodes
             for children in self.vectors[max_node]:
                 match_node.discard(children[0])
+        debug(f"[DOMINATING SET] {len(res)} nodes in the dominating set")
+        info("[DOMINATING SET] Done.")
         return res
 
     def save(self, path: str):
+        info("[SAVE] Saving...")
         with open(path, "w") as f:
+            info("[SAVE] Writing...")
+            # write the number of nodes in first line, and if the graph is oriented in second line
             f.write(f"{len(self.vectors)}\n")
             f.write(f"{ 'true' if self.is_oriented else 'false'}\n")
+            # write the vectors
             for source, vectors in enumerate(self.vectors):
                 for destination in vectors:
-                    f.write(f"{source}->{destination}\n")
+                    f.write(f"{source}->{destination[0]}\n")
+        info("[SAVE] Done.")
 
     @staticmethod
-    def load(path: str):
+    def load(path: str) -> "Graph":
+        info("[LOAD] Loading...")
         with open(path) as f:
             lines = f.readlines()
+            info("[LOAD] Reading...")
             nb_node = int(lines[0])
             is_oriented = lines[2].lower().strip() == "true"
+            info("[LOAD] nodes: %s, oriented: %s", nb_node, is_oriented)
+            info("[LOAD] Creating...")
             vector = [set() for _ in range(nb_node)]
             for i in range(2, len(lines)):
                 line = lines[i]
                 source, destination = line.strip().split("->")
                 source, destination = int(source), int(destination)
                 vector[source].add((destination, None))
+            info("[LOAD] Done.")
             return Graph(is_oriented, None, vector)
 
     @staticmethod
     def generate_random(nb_node=None, nb_vector=None, is_oriented=False, is_not_oriented=False, is_weighted=False,
                         is_not_weighted=False) -> "Graph":
+        info("[RANDOM GRAPH] Initializing...")
+        # If the parameters are not defined, they are asked to the user
         if nb_node is None:
             nb_node = int(input("nb_node: "))
         if nb_vector is None:
@@ -82,13 +107,13 @@ class Graph:
         if not is_weighted and not is_not_weighted:
             is_weighted = input("is_weighted: [Y/n]") == "n"
         graph = Graph(is_oriented, None, [])
-        info("Adding nodes")
+        info("[RANDOM GRAPH] Adding nodes...")
         for _ in range(nb_node):
             graph.add_node()
-        info("Adding vectors")
+        info("[RANDOM GRAPH] Adding vectors...")
         for _ in range(nb_vector):
             while not graph.add_vector(randint(0, nb_node - 1), randint(0, nb_node - 1),
                                        randint(0, 100) if is_weighted else None):
                 pass
-
+        info("[RANDOM GRAPH] Done.")
         return graph
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from graph import Graph
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
 graph = Graph.generate_random(is_not_oriented=True, nb_vector=15000, nb_node=1000, is_not_weighted=True)
-# graph = Graph.load("test")
-print(res := graph.dominating_set())
-print(len(res))
+# number of lines in the file is 2*nb_vector + 2 (x2 because the undirected graph is implemented as a directed graph)
+graph.save("graph.txt")
+# graph = Graph.load("graph.txt")
+print("Result:", res := graph.dominating_set())
+print("length:", len(res))
